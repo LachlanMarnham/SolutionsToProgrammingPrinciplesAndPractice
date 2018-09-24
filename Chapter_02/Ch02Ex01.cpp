1ab6dc3c8503d3abbf6e91023435029553a0d061
LachlanMarnham <lachlan.marnham@gmail.com>
2018-09-24 01:05:38 +0100

    Added solutions to Chapter 2

---
+++ Chapter_02/Ch02Ex01.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, programming!" << endl;
+    cout << "Here we go!" << endl;
+    
+    return 0;
+}
---
+++ Chapter_02/Ch02Ex02.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    // Get out of chair
+    cout << "Stand up." << endl;
+
+    // Move around table
+    cout << "Turn right." << endl;
+    cout << "Take three paces forwards." << endl;
+    cout << "Turn left." << endl;
+    cout << "Take two paces Forwards." << endl;
+    cout << "Turn left." << endl;
+    cout << "Take one pace forward." << endl;
+    cout << "Turn right." << endl;
+
+    // Move to corridor
+    cout << "Take five paces forward." << endl;
+    cout << "Turn right." << endl;
+
+    // Move to the second floor
+    cout << "Step up onto the first step." << endl;
+    cout << "While still on a step, continue stepping onto next step." << endl;
+
+    // Find bathroom door
+    cout << "Take two paces forwards." << endl;
+    cout << "Turn right." << endl;
+
+    // Open door
+    cout << "Grab door knob." << endl;
+    cout << "Rotate door knob counter-clockwise." << endl;
+    cout << "Push door." << endl;
+
+    // Enter bathroom
+    cout << "Take two paces forwards." << endl; 
+
+    return 0;
+}
